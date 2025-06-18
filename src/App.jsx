191b51f1db0be2b3c66917dec5cad2ea6c56c279
DDale21 <christiandalepancho@gmail.com>
2025-06-18 21:45:17 +1200

    Feat: Made intro and product grid responsive for multiple displays. Adjusted styling for project cards. Removed some tags.

--- src/App.jsx
+++ src/App.jsx
@@ -9,24 +9,22 @@ import { ProjectCard } from './components/ProjectCard'
 export default function App() {
   return (
     <>
-      {/* Intro Section */}
       <section className="intro-section">
         <div className="intro-inner">
           <div className="intro-content">
-            <div className="about">
-              <h1>Christian Dale Pancho</h1>
-              <p>Graduate Software Developer passionate about clean code, full-stack development, and problem-solving.</p>
-            </div>
             <div className="profile">
               <img src={profilePic} alt="Christian Dale Pancho" />
+            </div>
+            <div className="about">
+              <h1>Christian Dale Pancho</h1>
               <p className="profile-position">Graduate Software Developer</p>
               <p className="profile-expertise">Focusing on Full-Stack development</p>
+              <p className="profile-desc">Graduate Software Developer passionate about clean code, full-stack development, and problem-solving.</p>
             </div>
           </div>
         </div>
       </section>
 
-      {/* Experience & Projects Section */}
       <section className="experience-section">
         <div className="experiences">
           <h1>Experience</h1>
@@ -53,7 +51,6 @@ export default function App() {
         </div>
       </section>
 
-      {/* Footer Section */}
       <footer className="footer-container">
         <div className="footer">
           <div className="footer-inner">
