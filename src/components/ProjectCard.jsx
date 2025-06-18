358f11d94ff98dbebf82ecce8984e10cd2c57d4c
DDale21 <christiandalepancho@gmail.com>
2025-06-18 21:47:59 +1200

    Fix: Misssing commit for 191b51f.

--- src/components/ProjectCard.jsx
+++ src/components/ProjectCard.jsx
@@ -4,9 +4,16 @@ export function ProjectCard({ image, title, desc }) {
   return (
     <>
       <div className="project-card">
-        { image && <img src={image} alt={title} className="project-image"/> }
-        <h3 className="project-title">{title}</h3>
-        <p>{desc}</p>
+        <div className="project-image-wrapper">
+          { image && <img src={image} alt={title} className="project-image"/> }
+        </div>
+        <div className="project-body">
+          <h3 className="project-title">{title}</h3>
+          <p>{desc}</p>
+          <div class="project-tags">
+            <span>React</span><span>NodeJS</span>
+          </div>
+        </div>
       </div>
     </>
   );
